85225e1432a152039e1a17358b71c3f62d03084f
Taylor Nation <tnation14@gmail.com>
2017-06-06 16:10:40 -0400

    Output remote state
    Export information relevant to ECS tasks for use with other projects.

---
+++ terraform/outputs.tf
@@ -0,0 +1,26 @@
+output "vpc_id" {
+  value = "${module.vpc.id}"
+}
+
+output "logs_bucket_id" {
+  value = "${aws_s3_bucket.logs.id}"
+}
+
+output "public_hosted_zone_id" {
+  value = "${aws_route53_zone.external.id}"
+}
+
+output "public_subnet_ids" {
+  value = "${module.vpc.public_subnet_ids}"
+}
+output "container_instance_name" {
+  value = "${module.container_service_cluster.name}"
+}
+
+output "container_instance_security_group_id" {
+  value = "${module.container_service_cluster.container_instance_security_group_id}"
+}
+
+output "container_instance_ecs_for_ec2_service_role" {
+  value = "${module.container_service_cluster.container_instance_ecs_for_ec2_service_role}"
+}
